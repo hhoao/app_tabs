6dcb281b67f98e5674adccff78a15506d0190c48
xianxing <xianxing@dtstack.com>
2024-08-15 08:24:14 +0800

    [feat] support gnome 46

--- package.sh
+++ package.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
-include_files=(
-src
-stylesheet.css
-extension.js
-metadata.json
-prefs.js
-LICENSE
-schemas
+ignore_files=(
+'.git/*'
+'.idea/*'
+'./.gitignore'
+'.github/*'
 )
 
-args=""
-for include_file in "${include_files[@]}"; do
-  if [ -e "$include_file" ]; then
-    args="${args} ${include_file}"
-  fi
-done
-
-if [[ -n $1 ]]; then
+if [ ! -z $1 ]; then
   package_name=$1
 else
   package_name=$(date +'%Y%m%d%H%M%S')
 fi
 
-args="-r ${package_name}.zip ${args}"
-
-zip ${args}
+zip -x "${ignore_files[@]}" -r "${package_name}".zip .
